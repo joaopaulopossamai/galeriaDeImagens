521b8bd38857c1992718925bf79397fc07c29f07
joao paulo possamai <37601838+joaopaulopossamai@users.noreply.github.com>
2024-02-22 20:32:40 -0300

    .

--- scripts.js
+++ scripts.js
@@ -1,5 +1,26 @@
 $(document).ready(function () {
+  const baseImagens = {
+    todas: ['imagem3.jpg', 'imagem.jpg', 'cidade.jpg', 'animal.jpg'],
+    natureza: ['imagem3.jpg', 'imagem1.jpg', 'cidade1.jpg', 'animal1.jpg'],
+    cidade: ['imagem3.jpg', 'imagem2.jpg', 'cidade2.jpg', 'animal2.jpg'],
+    animais: ['imagem3.jpg', 'cidade3.jpg', 'animal3.jpg', 'imagem.jpg'],
+  };
+
+  function carregaImagens(categoria) {
+    const imagens = baseImagens[categoria];
+    const boxImagens = $('body').find('.box-imagens');
+    boxImagens.empty();
+    console.log(categoria);
+
+    imagens.forEach((img) => {
+      boxImagens.append('<div class="imagem-item"><img src="imagens/' + img + '" /></div>');
+    });
+  }
+
   $('.botao-categoria').on('click', function () {
-    alert(123);
+    $('body').find('.botao-categoria').removeClass('active');
+    $(this).addClass('active');
+    const categoria = $(this).data('categoria');
+    carregaImagens(categoria);
   });
 });
